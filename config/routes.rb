4f8b61439fca5e199c3e1c039efd169ba472b9d2
ssou00 <s0uta12.3no3@gmail.com>
2025-05-10 02:00:23 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
- 
-  root "application#hello"
 end
