8a4da0baa415938c3733ab95fb6344368ee67cf4
ssou00 <s0uta12.3no3@gmail.com>
2025-05-30 05:12:04 +0900

    Improve config_URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample_app.onrender.com'
+  host = 'sample-app-ap27.onrender.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
